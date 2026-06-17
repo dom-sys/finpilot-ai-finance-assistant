076eadb7b6d0e1e26e10735d6cf1825ba4c04fa6
domenica herrera <hdomenica16@gmail.com>
2026-06-16 20:37:09 -0400

    Add spending by category analysis

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ expenses = abs(df[df["Amount"] < 0] ["Amount"].sum())
 
 print(f" Total Income: ${income:.2f}")
 print(f" Total Expenses:  ${expenses:.2f}")
+
+#check spending by category
+
+print("\n Spending by Category: ")
+category_totals = (df[df["Amount"]< 0]
+.groupby("Category")["Amount"].sum()
+)
+
+print(abs(category_totals))
