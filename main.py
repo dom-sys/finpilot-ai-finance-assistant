229542936594c63061a27a2babfe1efb31b4805d
domenica herrera <hdomenica16@gmail.com>
2026-06-16 20:11:15 -0400

    Add transaction CSV loader

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
-print("FinPilot started")
+df = pd.read_csv("data/transactions.csv")
+print(df)
+
+#print number of transactions
+#print the total income
+#print total expenses
+
+
+print("\n Total Transactions:" , len(df))
+
+income = df[df["Amount"] > 0]["Amount"].sum()
+expenses = abs(df[df["Amount"] < 0] ["Amount"].sum())
+
+print(f" Total Income: ${income:.2f}")
+print(f" Total Expenses:  ${expenses:.2f}")
